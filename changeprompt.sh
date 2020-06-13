e754325353f9c623b520e4e188819e522b9a357f
csamiselo <chisambwe.samiselo@gmail.com>
2020-06-13 03:09:50 +0200

    Progress on bash scripts

--- changeprompt.sh
+++ changeprompt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Csamiselo Jun 2020
 #csamiselo@github.com
-  #simple program to change the shell prompt
+  #Simple program to change the shell prompt
 
 #The PS1 environment variables can be used to modify the prompt
 echo "Your promt has been changed"
-cat ~/.bashrc | grep PS1
-${PS1}="PROMPT>"
+cat ~/.bashrc | grep PS1;PS1="PROMPT>"
+exit
