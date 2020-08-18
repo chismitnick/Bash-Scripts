68b89cfd978c483ce090084f034b5cf481b12d48
csamiselo <chisambwe.samiselo@gmail.com>
2020-08-18 15:37:34 +0200

    Modified clear RAM script

--- clearcache.sh
+++ clearcache.sh
@@ -3,4 +3,19 @@
 #csamiselo@github.com
   #Script to clear up RAM cahe using the sync command 
 
-sudo su ; sudo sync echo 1 > /proc/sys/vm/drop_caches && sudo sync;  echo 2 > /proc/sys/vm/drop_caches && sudo sync;  echo 3 > /proc/sys/vm/drop_caches
+#!/bin/bash
+
+#Calculates the current percentage of free RAM available in system
+_ram_avl=`free | grep Mem | awk '{print $4/$2 * 100.0}' | awk -F'.' '{print $1}'`
+
+#If free percentage is less than equal to 15% of total RAM trigger the cleanup
+
+if [ $_ram_avl -le 15 ]; then
+
+echo "RAM available in system is below 15 percentage clearing cache and freeing up memory";
+echo
+free && sync && echo 3 > /proc/sys/vm/drop_caches && free
+echo
+sudo sysctl vm.drop_caches=1
+echo
+fi;
