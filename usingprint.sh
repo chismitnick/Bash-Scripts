772f1618e93839cebba269ee380c49ffced09415
chismitnick <chisambwe.samiselo@gmail.com>
2020-12-10 08:49:59 +0200

    Formatting output with printf()

---
+++ usingprint.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+# Using the printf() function
+#C.Samiselo Dec 2020
+
+#We can use formatted strings with printf
+
+printf "%-5s %-10s %-4s\n" Number Name Amount
+printf "%-5s %-10s %-4s\n" 1  Charles 200
+printf "%-5s %-10s %-4s\n"  2  Emily    600
+printf "%-5s %-10s %-4s\n" 3 Musa  770
