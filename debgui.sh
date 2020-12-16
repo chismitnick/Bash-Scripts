e4f1d7a47a2233ff578b9de372fa2b9f4c28dbbe
chismitnick <chisambwe.samiselo@gmail.com>
2020-12-16 11:45:38 +0200

    Using zenity library to add GUI to script

---
+++ debgui.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#chismitnick December 2020
+#chismitnick@github.com
+#Script to display GUI window or warning/ message once script has completed executing
+# using Zenity command line tool which helps us to display GTK dialog boxes.
+
+ zenity --info --title="Installation complete" --text="Please restart the system" --no-wrap
+
+
