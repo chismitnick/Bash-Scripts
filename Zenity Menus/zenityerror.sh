ac53ccdeba7caa2e6ef2813c0c37d0a179636156
chismitnick <chisambwe.samiselo@gmail.com>
2020-12-17 18:55:23 +0200

    Progress: Using zenity to display menus using bash scripts

---
+++ Zenity Menus/zenityerror.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+#chismitnick@github.com 2020
+#Display error message using zenity library
+
+zenity --error --title="Kernel Corrupted" --text="Kindly re-run diagnostics engine" --no-wrap
